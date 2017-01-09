501fe69e4e13125e75452346fcf1741b9b63fe84
daidodo <daidodo@gmail.com>
2017-01-09 10:01:09 +0800

    fix ntp service auto start;

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -2,33 +2,31 @@
 
 # vim & tags
 /bin/bash -c "sed -i 's#^set path=.*#&`./gen_systags.sh`#' /tmp/1.vimrc" && \
-    mv /tmp/1.vimrc ~/.vimrc && mkdir -p ~/.vim/spell && mv sysdict.ascii.spl ~/.vim/spell/ && \
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && \
-    vim +PluginInstall +qall
-
-# ntpd
-mv /tmp/ntp.conf /etc/ && update-rc.d ntp defaults
+  mv /tmp/1.vimrc ~/.vimrc && mkdir -p ~/.vim/spell && mv sysdict.ascii.spl ~/.vim/spell/ && \
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && \
+  vim +PluginInstall +qall
 
 # user config
 useradd dozerg -m -g root -s /bin/bash && \
-    echo 'dozerg:dozerg' | chpasswd && \
-    echo 'dozerg  ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/dozerg && \
-    mv ~/tags ~/.vimrc ~/.vim ~dozerg/ && \
-    mv /tmp/1.bashrc ~dozerg/.bashrc && \
-    mv /tmp/1.ycm_extra_conf.py ~dozerg/.ycm_extra_conf.py && \
-    mv /tmp/1.gitconfig ~dozerg/.gitconfig && \
-    cd ~dozerg/.vim/bundle/YouCompleteMe && ./install.py --clang-completer --system-libclang && \
-    git clone --recursive https://github.com/daidodo/marine_doc.git ~dozerg/work/marine_doc && \
-    git clone --recursive https://github.com/daidodo/misc.git ~dozerg/work/misc && \
-    chown -R dozerg /home/dozerg
+  echo 'dozerg:dozerg' | chpasswd && \
+  echo 'dozerg  ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/dozerg && \
+  mv ~/tags ~/.vimrc ~/.vim ~dozerg/ && \
+  mv /tmp/1.bashrc ~dozerg/.bashrc && \
+  mv /tmp/1.ycm_extra_conf.py ~dozerg/.ycm_extra_conf.py && \
+  mv /tmp/1.gitconfig ~dozerg/.gitconfig && \
+  cd ~dozerg/.vim/bundle/YouCompleteMe && ./install.py --clang-completer --system-libclang && \
+  git clone --recursive https://github.com/daidodo/marine_doc.git ~dozerg/work/marine_doc && \
+  git clone --recursive https://github.com/daidodo/misc.git ~dozerg/work/misc && \
+  chown -R dozerg /home/dozerg
 
-# fix timezone, optional
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+# time-sync and timezone
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && \
+  mv /tmp/ntp.conf /etc/
 
 # sshd
-mkdir -p /var/run/sshd
-sed -ri 's/^PermitRootLogin\s+.*/PermitRootLogin no/' /etc/ssh/sshd_config
-sed -ri 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
+mkdir -p /var/run/sshd && \
+  sed -ri 's/^PermitRootLogin\s+.*/PermitRootLogin no/' /etc/ssh/sshd_config && \
+  sed -ri 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
 
 # clean up
 rm -f /tmp/*.sh /tmp/1.*
