e14cb3c235c5a7d5c3ad4667201a7683aaf2f58f
daidodo <daidodo@gmail.com>
2017-04-19 23:12:56 +0800

    Fix bug;

--- ubuntu/go-get.sh
+++ ubuntu/go-get.sh
@@ -11,9 +11,9 @@ go get -v -u 'github.com/klauspost/asmfmt/cmd/asmfmt'
 go get -v -u 'github.com/fatih/motion'
 go get -v -u 'github.com/zmb3/gogetdoc'
 go get -v -u 'github.com/josharian/impl'
-go get -v -u 'github.com/golang/protobuf/{proto,protoc-gen-go}'
+go get -v -u 'github.com/golang/protobuf/...'
 go get -v -u 'github.com/go-sql-driver/mysql'
-
 go get -v -u 'github.com/stretchr/testify'
+
 go get       'github.com/daidodo/testa'
 go get       'github.com/daidodo/overlord'
