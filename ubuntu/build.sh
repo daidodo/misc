191b651842a7994583e0122a6afb6076d9938147
Zhao DAI <daidodo@gmail.com>
2024-06-28 15:34:32 +0100

    Fix image name.

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -4,4 +4,4 @@
 docker build --force-rm -t dev -f Dockerfile .
 
 # run
-docker run -d -p 127.0.0.1:22222:22 -p 127.0.0.1:6060:6060 -p 127.0.0.1:8080:8080 --name dev -h dev daidodo/dev
+docker run -d -p 127.0.0.1:22222:22 -p 127.0.0.1:6060:6060 -p 127.0.0.1:8080:8080 --name dev -h dev dev
