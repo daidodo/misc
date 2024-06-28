9e95f961325b7ba4c7ea11dd3d99b83497a56812
Zhao DAI <daidodo@gmail.com>
2024-06-28 13:32:56 +0100

    Fix issues.

--- ubuntu/apt-get.sh
+++ ubuntu/apt-get.sh
@@ -6,9 +6,9 @@ apt-get -y update && \
   gcc g++ gdb gdb-doc strace ltrace \
   golang-go golang-golang-x-tools \
   clang libclang-dev \
-  python-dev python3-dev \
+  python3-dev \
   doxygen doxygen-doc texlive-latex-base \
-  npm nodejs-legacy nodejs \
+  npm nodejs \
   manpages man-db manpages-dev \
   vim exuberant-ctags dos2unix \
   automake make cmake build-essential \
